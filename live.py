4bfc0c99aff5749c6ac2d7c9967616799e4d6ab4
royikk <60670650+royikk@users.noreply.github.com>
2020-02-04 22:00:24 +0200

    Update live.py

--- live.py
+++ live.py
@@ -1,5 +1,5 @@
 def welcome(name):
-    return ("Hello " + name + " and welcome to the World of Games (WoG). \nHere you can find many cool games to play.")
+    rreturn ("Hello " + name + " and welcome to the World of Games (WoG). \nHere you can find many cool games to play."))
 
 def load_game():
     from CurrencyRouletteGame import playCurrency
